7960a27a0ae6a01d1cd6dc3946c9654e99595050
Hamlet León <hamletleon@gmail.com>
2018-10-18 23:11:59 -0400

    updating a lot of things...

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use('/users', UserController);
 
 app.get('/get.cgi', (req, res) => {
     if (req.query["req"] == "ping")
-        res.send("#PING|600002|192.168.0.111|1.5.5|Hamlet Leon 1|255.0.0.0|10.5.9.8|0|239|000000000000|6/30/2017 12:59:59 PM|N|shows/My Show|1|1|0.0.0.0|0.0.0.0|0")
+        res.send("#PING|600014|192.168.0.114|1.5.5|Hamlet 4|255.0.0.0|10.5.9.8|0|239|000000000000|6/30/2017 12:59:59 PM|N|shows/My Show|1|1|0.0.0.0|0.0.0.0|0")
     else
         res.send("<h1>Unknown Command</h1>")
 })
